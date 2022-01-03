511c69d5bc731ad558cc263f0a199760186a6e52
RyoKitatani <ryo0929m@gmail.com>
2022-01-03 17:23:20 +0900

    finished_3-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :restraunts do
+      resources :restaurants do
         resources :foods, only: %[index]
       end
       resources :line_foods, only: %[index, create]
