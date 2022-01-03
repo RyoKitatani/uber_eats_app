079ae1b712d86f1f21b7e1cbed073f81bc24c4ee
RyoKitatani <ryo0929m@gmail.com>
2022-01-03 21:52:28 +0900

    finished_4-4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,40 +13,39 @@ import { Foods } from './containers/Foods.jsx';
 import { Orders } from './containers/Orders.jsx';
 
 function App() {
-return (
-  <Router>
-    <Switch>
-      {/* 店舗一覧ページ */}
-      <Route
-      exact
-      path="/restaurants">
-        <Restaurants/>
-      </Route>
-      {/* フード覧ページ */}
-      <Route
-      exact 
-      path="/foods">
-        <Foods/>
-      </Route>
-      {/* 注文ページ */}
-      <Route
-      exact
-      path="/orders">
-        <Orders/>
-      </Route>
-      <Route
-      exact
-      path="/restaurants/:restaurantsId/foods"
-      render = {({match}) =>
-        <Foods
-        match = {match}
+  return (
+    <Router>
+      <Switch>
+        {/* 店舗一覧ページ */}
+        <Route
+        exact
+        path="/restaurants">
+          <Restaurants/>
+        </Route>
+        {/* フード覧ページ */}
+        <Route
+        exact 
+        path="/foods">
+          <Foods/>
+        </Route>
+        {/* 注文ページ */}
+        <Route
+        exact
+        path="/orders">
+          <Orders/>
+        </Route>
+        <Route
+        exact
+        path="/restaurants/:restaurantsId/foods"
+        render = {({match}) =>
+          <Foods
+          match = {match}
+          />
+        }
         />
-      }
-      />
-    </Switch>
-  </Router>
-)
-
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
