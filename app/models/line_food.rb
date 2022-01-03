f05186cb741cec82c809c4425f7d9d94fa488bb2
RyoKitatani <ryo0929m@gmail.com>
2022-01-03 17:59:33 +0900

    finished_3-5

--- app/models/line_food.rb
+++ app/models/line_food.rb
@@ -1,13 +1,13 @@
 class LineFood < ApplicationRecord
   belongs_to :food
-  belongs_to :restaunrant
+  belongs_to :restaurant
   belongs_to :order, optional: true
   # optiona: true => 外部キーのnilを許容する
 
   validates :count, numericality: {greater_than: 0}
 
   scope :active, -> {where(active: true)}
-  scope :other_restraunt, -> (pickup_restraunt_id) {(where.not(restaunrant_id: pickup_restraunt_id)}
+  scope :other_restaurant, -> (pickup_restaurant_id) {(where.not(restaurant_id: pickup_restraunt_id)}
 
   def total_amount
     food.price * count
